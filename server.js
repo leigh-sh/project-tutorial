4cd50a7aa7262ad5d1e82a5be64ef6c1b763409b
Leigh Shaniv <leigh@Leighs-MacBook-Air.local>
2016-04-17 19:11:53 +0300

    added server file, that has the /hello get route that prints sucess: true

---
+++ server.js
@@ -0,0 +1,13 @@
+var restify = require('restify')
+var redis = require('redis')
+var client = redis.createClient();
+var server = restify.createServer();
+
+server.get('/hello', function(req, res, next){
+    res.send({ success: true, session: req.session });
+    return next();
+});
+
+server.listen(8000, function() {
+console.log('listening at %s', server.url);
+});
