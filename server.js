8a7c0881f632b40f3842039c5ee410cc35862b8c
Leigh Shaniv <leigh@Leighs-MacBook-Air.local>
2016-04-18 11:27:16 +0300

    Added counter for each time 'hello' is called and insert it into redis

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 var restify = require('restify');
 var redis = require('redis');
+var fs = require('fs');
 var client = redis.createClient();
 var server = restify.createServer();
 
+client.on('error', function (err) {
+    console.log('Error ' + err);
+});
+
 server.get('/hello', function(req, res, next){
+    fs.writeFile("/tmp/log", "test", function(err) {
+    if(err) {
+        return console.log(err);
+    }
+    client.incr('hello');
+   
+    client.get('hello', function(err, reply){ console.log(reply);});
     res.send({ success: true, session: req.session });
     return next();
+});
 });
 
 server.listen(8000, function() {
